75e84949a5a5daf8e6f3a99b5429a59b3627485d
giovani moukoko <giovanimoukoko@gmail.com>
2026-05-06 19:52:27 +0100

    Add publications app with initial models, views, and templates; update settings and URLs

--- users/views.py
+++ users/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login
 
 
 def login_view(request):
+    
+    if request.method== "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+    
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return render(request, "users/login.html", context={"error":"Nom d'utilisateur ou mot de passe incorect"})
     return render(request, "users/login.html")
 
 def register_view(request):
@@ -15,6 +28,13 @@ def register_view(request):
         password =  request.POST["password"]
         print(last_name, first_name, email,username, password)
         print("La méthode POST")
+
+        if User.objects.filter(email=email).count() > 0:
+            return render(request, "users/register.html", context={"error":"Adresse mail déjà attribuée"})
+        
+        if User.objects.filter(username=username).count() > 0:
+            return render(request, "users/register.html", context={"error":"Nom d'utilisateur déjà existant"})
+
 
         user = User.objects.create_user(
             username=username, 
