b8b92f1e7739f2127cd1ae07977a5035d8a3d115
giovani moukoko <giovanimoukoko@gmail.com>
2026-06-03 19:32:07 +0100

    Add Publication model and admin configuration; update templates and migrations
    - Created Publication model with fields for user, title, content, image, created_at, and updated_at.
    - Registered Publication model in admin with custom admin class to display specific fields.
    - Updated index.html template to show user's full name.
    - Added initial migration for Publication model.

--- publications/admin.py
+++ publications/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Publication
 
-admin.site.register(Publication)
+#admin.site.register(Publication)
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+
+    list_display = ["title","user","created_at","updated_at"]
+
+ 
--- publications/models.py
+++ publications/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+
 class Publication(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField(max_length=255)
@@ -7,3 +8,14 @@ class Publication(models.Model):
     image=models.ImageField(upload_to='publications', null=True, blank=True)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} {self.user}"
+    
+    class Meta:
+        verbose_name = "Général"
+        verbose_name_plural = "Généraux"
+
+      #  ordering = ["title"]
+      
+
